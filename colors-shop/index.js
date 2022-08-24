7f244cc60f0db31207257a75a7c03989b41ba04e
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2022-08-24 16:06:58 +0300

    feat: add options

--- colors-shop/index.js
+++ colors-shop/index.js
@@ -1,6 +1,39 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Привет, Vue!'
-    }
-  })
+const selectButton = document.querySelector('.custom-button');
+const overflow = document.querySelector('.overflow')
+const optionList = document.querySelector('.custom-list')
+const body = document.querySelector('body');
+const options = Array.from(document.querySelectorAll('.custom-item'))
+selectButton.addEventListener('click', openOptions)
+
+function openOptions(){
+overflow.style.display = 'block';
+optionList.style.display = 'block';
+body.style.overflow = 'hidden';
+}
+
+optionList.addEventListener('click', selectOptions)
+
+function selectOptions(event){
+    console.log(options)
+if(event.target.textContent){
+    removeClass()
+    event.target.classList.add('custom-active')
+    selectButton.innerHTML = event.target.textContent
+}
+closeOptions()
+}
+
+function removeClass(){
+    options.map(function (item) {
+if (item.classList.contains('custom-active')){
+    item.classList.remove('custom-active')
+}
+    })
+}
+
+function closeOptions(){
+    overflow.style.display = 'none';
+optionList.style.display = 'none';
+body.style.overflow = 'scroll';
+}
+
